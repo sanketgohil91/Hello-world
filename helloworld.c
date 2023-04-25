0f848322e98ec8402bd5eefb1d7351fe32df171d
Gohil Sanket <gohilsanket13579@gmail.com>
2023-04-25 13:50:38 +0530

    secound commit

--- helloworld.c
+++ helloworld.c
@@ -1,39 +1,6 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-char* get_string(char *prompt);
-
-
-int main()
+int main(int argc, char const *argv[])
 {
-    char *name = get_string("Enter your name : ");
-    printf("Welcome %s",name);
+    printf("HELLO WORLD");
     return 0;
-}
-
-char* get_string(char *prompt)
-{
-    int count = 0;
-    char *s = malloc(1 * sizeof(char));
-    char c;
-    printf("%s", prompt);
-    while (1)
-    {
-        c = getchar();
-        count++;
-        s = realloc(s, count * sizeof(char));
-
-        if (c == '\n')
-        {
-            s[count - 1] = '\0';
-            break;
-        }
-        else
-        {
-            s[count - 1] = c;
-        }
-    }
-
-    return s;
 }
