15e47ba8a2fa292814d2c8331311268897673648
Gohil Sanket <gohilsanket13579@gmail.com>
2023-04-25 13:41:56 +0530

    added First.c

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,39 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+
+char* get_string(char *prompt);
+
+
 int main()
 {
-    printf("Hello_world");
+    char *name = get_string("Enter your name : ");
+    printf("Welcome %s",name);
     return 0;
+}
+
+char* get_string(char *prompt)
+{
+    int count = 0;
+    char *s = malloc(1 * sizeof(char));
+    char c;
+    printf("%s", prompt);
+    while (1)
+    {
+        c = getchar();
+        count++;
+        s = realloc(s, count * sizeof(char));
+
+        if (c == '\n')
+        {
+            s[count - 1] = '\0';
+            break;
+        }
+        else
+        {
+            s[count - 1] = c;
+        }
+    }
+
+    return s;
 }
